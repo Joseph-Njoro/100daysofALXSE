6254d538135f6ddde34d8658ec8a920ae00cee82
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-12 09:33:45 +0300

    ANSI escape characters

--- Progress/progress.py
+++ Progress/progress.py
@@ -1,30 +1,38 @@
 import datetime
 import os
 
+current_datetime = datetime.datetime.now()
 start_date = datetime.date(2024, 3, 13)
-current_date = datetime.date.today()
-day_number = (current_date - start_date).days + 1
+day_number = (current_datetime.date() - start_date).days + 1
 days_remaining = 100 - day_number
 
-day_name = current_date.strftime("%A ")
-date_str = current_date.strftime("%d %B ")
+day_name = current_datetime.strftime("%A ")
+date_str = current_datetime.strftime("%d %B %Y, %I:%M %p")
 
 progress_message = f"{day_name}{date_str}: Day {day_number} of 100: {days_remaining} more to go\n"
 
 # Define the path to the progress log file
 log_file_path = "progress_log.txt"
 
-# Redudancy check
+# Redundancy check
 if os.path.exists(log_file_path):
     with open(log_file_path, "r") as file:
-        last_line = file.readlines()[-1]
-    last_date = datetime.datetime.strptime(last_line.split(":")[0], "%A %d %B ")
-    if last_date.date() == current_date:
-        # If the log for the current day already exists, exit the script
-        exit()
+        lines = file.readlines()
+    if lines:
+        last_line = lines[-1].strip()  # Remove leading/trailing whitespace
+        if last_line:
+            try:
+                last_date_time = datetime.datetime.strptime(last_line.split(":")[0], "%A %d %B %Y, %I:%M %p")
+                if last_date_time.date() == current_datetime.date():
+                    # If the log for the current day already exists, exit the script
+                    exit()
+            except ValueError:
+                # Handle invalid date format in the last line
+                pass
 
 # Write progress message to the log file
 with open(log_file_path, "a") as file:
     file.write(progress_message)
 
-print(progress_message)
+# Print progress text using ANSI escape codes
+print(f"\033[1m\033[4m{progress_message}\033[0m")
