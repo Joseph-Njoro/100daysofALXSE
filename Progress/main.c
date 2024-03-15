0bd455f9b5036dcb40a98fed46ec6d51c8c75768
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-15 23:21:15 +0300

    arithmetic.c main.c

---
+++ Progress/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "arithmetic.h"
+
+int main() {
+    int a = 10, b = 5;
+
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", subtract(a, b));
+    printf("Multiplication: %d\n", multiply(a, b));
+    printf("Division: %d\n", divide(a, b));
+
+    return 0;
+}
