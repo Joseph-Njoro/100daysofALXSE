85fcab49af1cde7480be8a06cc73f802ff5215bf
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-15 22:21:09 +0300

    final fix

--- greet.c
+++ greet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-void greet(char a[]);
+void greet(char a[1]);
 
-int main(){
-    char[] a;
-    printf("Enter a name");
-    scanf("%s", &a);
+int main()
+{
+    char a[100];
+    printf("Enter a name: ");
+    scanf("%s", a);
     greet(a);
     return 0;
 }
 
-void greet(char a){
+void greet(char a[]){
     printf("Hi %s\n", &a);
 }
