63f7e43c7b016a6224ab716fa4261660769bce8b
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-16 23:24:31 +0300

    basic string operations

---
+++ pointers/string_basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declaration and initialization of a string
+    char str1[] = "Hello, world!";
+
+    // Declaration and initialization of a string with explicit size
+    char str2[10] = "Hello";
+
+    // Print the strings
+    printf("String 1: %s\n", str1)
+    printf("String 2: %s\n", str2)
+
+    return 0;
+}
