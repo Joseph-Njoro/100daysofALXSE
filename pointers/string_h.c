71c4ce1affac6e5739a8e1fd5058a16199832214
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-16 23:25:44 +0300

    using string.h functions

---
+++ pointers/string_h.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "Hello";
+    char str2[20];
+
+    // Copying a string
+    strcpy(str2, str1);
+    printf("Copied string: %s\n", str2);
+
+    // Concatenating strings
+    strcat(str2, " world!");
+    printf("Concatenated string: %s\n", str2);
+
+    // String length
+    printf("Length of string: %zu\n", strlen(str2));
+
+    // Comparing strings
+    if (strcmp(str1, str2) = 0) {
+        printf("Strings are equal\n");
+    } else {
+        printf("Strings are not equal\n");
+    }
+
+    return 0;
+}
