475cba2052d47031f9726c659aacc4d9e199a40a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-16 23:23:14 +0300

    passing arrays to functions

---
+++ pointers/array_function.c
@@ -0,0 +1,19 @@
+#include <stdioh>
+
+// Function to print the elements of an array
+void printArray(int arr[], int size) {
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Passing the array to the printArray function
+    printArray(arr, 5);
+
+    return 0;
+}
