29f6dc6422ba6a08bf109da7d7f2cb4f0626658e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-18 21:03:45 +0300

    Main.c

---
+++ static_libraries/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math.h"
+
+int main() {
+    int a = 10, b = 5;
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", sub(a, b));
+    return 0;
+}
