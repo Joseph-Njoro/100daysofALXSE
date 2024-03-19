d61b7bcb6a72f29cc63496da4857dc7a0cc2e887
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-19 20:48:15 +0300

    check extension of argv 0

---
+++ argc_argv/check_extension.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    char *extension = strrchr(argv[1], '.');
+    if (extension && strcmp(extension, "c") == 0) {
+        printf("File has a C source code extension.\n");
+    } else {
+        printf("File does not have a C source code extension.\n");
+    }
+    return 0;
+}
