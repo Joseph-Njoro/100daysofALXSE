ccca0dd5f2d30298f4cf33c8524b1aef75861d69
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-19 20:42:42 +0300

    no of arguements

---
+++ argc_argv/arg_count.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <arg1> <arg2>\n", argv[0]);
+        return 1;
+    }
+    // Rest of the program
+    return 0;
+}
