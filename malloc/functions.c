1c8e20d92a276bd3f07c9f9035186a5b867a3480
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-21 23:48:14 +0300

    functions

---
+++ malloc/functions.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* allocate(int n) {
+    int *arr = (int*) malloc(n * sizeof(int));
+    return arr;
+}
+
+void fillArray(int *arr, int n) {
+    for (int i = 0; i < n; i+) {
+        scanf("%d", arr + i);
+    }
+}
+
+void printArray(int *arr, int n) {
+    for (int i = 0; i < n; i+) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr = allocate(n);
+    fillArray(arr n);
+    printArray(arr, n);
+
+    free(arr);
+
+    return 0;
+}
