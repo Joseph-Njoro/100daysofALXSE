718fdf2c81d4e3e0bddd1aba8d13ecf6155a14f2
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-21 23:45:28 +0300

    realock

---
+++ malloc/reallock.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr;
+    int *newptr;
+    int i, capacity = 1, size = 1;
+    int element;
+
+    ptr = (int*) malloc(capacity * sizeof(int));
+
+    printf("Addresses of previously allocated memory: \n");
+    for (i = 0; i < capacity; ++i) {
+        printf("%p ", ptr + i);
+    }
+
+    printf("\nEnter elements: ");
+    for (i = 0; i < capacity; ++i) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("\nEnter
