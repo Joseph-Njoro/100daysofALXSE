7435ec3e857229a5c738a794b781afda370c3d80
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-21 23:43:51 +0300

    array length

---
+++ malloc/variable.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, i
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int) malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory not allocated");
+        exit(0);
+    }
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The elements of the array are: ");
+    for (i = 0; i < n; i+) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+
+    return 0;
+}
