2c10e72c71f0946cd5ee5f8119cafa71dd29ab9d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-22 22:15:27 +0300

    basic ...

---
+++ Progress/Variadic_functions/basic_sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sum(int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    int total = 0;
+    for (int i = 0; i < count; i++) {
+        total += va-arg(args, int);
+    }
+
+    va-end(args);
+    return total;
+}
+
+int main() {
+    printf("Sum: %d\n", sum(3, 1, 2, 3));
+    printf("Sum: %d\n", sum(5, 10, 20, 30, 40, 50));
+    return 0;
+}
