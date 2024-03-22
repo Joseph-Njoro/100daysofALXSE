36a54180927efe0e9373cf7285e8552f0b5583c6
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-22 22:28:20 +0300

    5s

--- Progress/Variadic_functions/formating.c
+++ Progress/Variadic_functions/formating.c
@@ -11,7 +11,7 @@ void print_formatted_strings(const char format, ...) {
 }
 
 int main() {
-    print_formatted_strings("Welcome, %%s and %s!\n", "Alice", "Bob");
+    print_formatted_strings("Welcome, %s and %s!\n", "Alice", "Bob");
     print_formatted_strings("Today's weather forecast: %d%% chance of %s.\n", 80, "rain");
     return 0;
 }
