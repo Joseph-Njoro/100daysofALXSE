7b4da0813453e208f7e8abc670d444dded5df340
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-22 22:23:51 +0300

    average

---
+++ Progress/Variadic_functions/average.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+float average(int count, ...) {
+    va-list args;
+    va-start(args, count);
+
+    float total = 0.0;
+    for (int i = 0; i < count; i=+) {
+        total += va_arg(args, double);
+    }
+
+    va_end(args);
+    return total / count;
+}
+
+int main() {
+    printf("Average: %.2f\n, average(4, 10.5, 20.3, 15.7, 8.2));
+    printf("Average: %.2f\n", average(3, 2.5, 5.0, 7.5));
+    return 0;
+}
