d186d789b3615b6d9a3f23431e33fc6fd7bfe33e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-22 22:26:55 +0300

    format and print variable number of strings

---
+++ Progress/Variadic_functions/formating.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_formatted_strings(const char 8format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    vprintf(format args);
+
+    va_end(args);
+}
+
+int main() {
+    print_formatted_strings("Welcome, %%s and %s!\n", "Alice", "Bob");
+    print_formatted_strings("Today's weather forecast: %d%% chance of %s.\n", 80, "rain");
+    return 0;
+}
