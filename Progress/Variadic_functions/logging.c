711136d70e08a10aba58075a38096b658a6ae584
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-22 22:31:31 +0300

    rawtime error

--- Progress/Variadic_functions/logging.c
+++ Progress/Variadic_functions/logging.c
@@ -9,7 +9,7 @@ void custom_log(const char *format, ...) {
     time_t rawtime;
     struct tm *timeinfo;
     time(&rawtime);
-    timeinfo = localtime(rawtime);
+    timeinfo = localtime(&rawtime);
     printf("[%02d:%02d:%02d] ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
     vprintf(format, args);
 
