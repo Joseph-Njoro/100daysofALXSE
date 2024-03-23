8547be0e96c27dbca356ebe06d8f132b51b8b64d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-23 23:21:34 +0300

    variadic_functions

---
+++ Variadic_functions/minimum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int min_int(int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    int min = va_arg(args, int);
+    for (int i = 1; i < count; i++) {
+        int num = va_arg(args, int);
+        if (num < min) {
+            min = num;
+        }
+    }
+
+    va_end(args);
+    return min;
+}
+
+int main() {
+    printf("Min: %d\n", min_int(3, 10, 5, 20));
+    printf("Min: %d\n", min_int(5, 30, 15, 25, 10, 5));
+    return 0;
+}
---
+++ Variadic_functions/print_variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_numbers(int count, ...) {
+    va_list args;
+    va_start(args, count);
+
+    for (int i = 0; i < count; i++) {
+        printf("%d ", va_arg(args, int));
+    }
+
+    va_end(args);
+    printf("\n");
+}
+
+int main() {
+    print_numbers(3, 10, 20, 30);
+    print_numbers(5, 1, 2, 3, 4, 5);
+    return 0;
+}
