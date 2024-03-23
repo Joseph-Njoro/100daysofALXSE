824483775aecd134260915254b0e313dc19cdaa1
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-23 23:52:13 +0300

    minor fix

---
+++ Tic_tac_toe/game_logic.c
@@ -0,0 +1,6 @@
+#include "game_logic.h"
+
+int check_winner(char board[3][3]) {
+    // Check if there is a winner
+    // Implement the logic to check for a winner...
+}
---
+++ Tic_tac_toe/input.h
@@ -0,0 +1,6 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+void get_move(char board[3][3], int *row, int *col);
+
+#endif /* INPUT_H */
