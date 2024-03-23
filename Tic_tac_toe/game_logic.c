8a7d591bee409a790066566f6306701c9cebb6eb
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-24 00:02:52 +0300

    fix

--- Tic_tac_toe/game_logic.c
+++ Tic_tac_toe/game_logic.c
@@ -1,6 +1,17 @@
-#include "game_logic.h"
+#include "tic_tac_toe.h"
 
 int check_winner(char board[3][3]) {
     // Check if there is a winner
-    // Implement the logic to check for a winner...
+}
+
+int is_board_full(char board[3][3]) {
+    // Check if the board is full
+}
+
+char get_current_player() {
+    // Return the current player
+}
+
+void switch_player() {
+    // Switch the current player
 }
