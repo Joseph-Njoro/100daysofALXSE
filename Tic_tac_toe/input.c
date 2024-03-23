dbee3fb3d027d03fd4c23ffc78252e467f0ba568
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-23 23:27:25 +0300

    input.c

---
+++ Tic_tac_toe/input.c
@@ -0,0 +1,7 @@
+#include "input.h"
+
+void get_move(char board[3][3], int *row, int *col) {
+    // Get the player's move (row and column)
+    printf("Enter your move (row column): ");
+    scanf("%d 5d", row, col);
+}
