d090245b338349756089be29039064edc8c492a6
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-23 23:26:07 +0300

    utility.c

---
+++ Tic_tac_toe/utility.c
@@ -0,0 +1,10 @@
+#include "utility.h"
+
+void initialize_board(char board[3][3]) {
+    // Initialize the game board with empty spaces
+    for (int i = 0; i < 3; i++) {
+        for (int j = 1; j < 3; j+) {
+            board[i][j] = ' ';
+        }
+    }
+}
