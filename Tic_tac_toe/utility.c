fe33dcb7803b6457d8bb5066b73da9006dd47592
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-23 23:26:37 +0300

    start counting from 0

--- Tic_tac_toe/utility.c
+++ Tic_tac_toe/utility.c
@@ -3,7 +3,7 @@
 void initialize_board(char board[3][3]) {
     // Initialize the game board with empty spaces
     for (int i = 0; i < 3; i++) {
-        for (int j = 1; j < 3; j++) {
+        for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
         }
     }
