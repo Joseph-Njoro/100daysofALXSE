fd96aa9c3351c5a31d8b7e8136a6b2c018f5a271
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-24 00:23:47 +0300

    update board declaration

--- Tic_tac_toe/tic_tac_toe.h
+++ Tic_tac_toe/tic_tac_toe.h
@@ -8,5 +8,6 @@ int is_board_full(char board[3][3]);
 char get_current_player();
 void switch_player();
 void display_board(char board[3][3]);
+void update_board(char board[3][3], int row, int col);
 
 #endif /* TIC_TAC_TOE_H */
