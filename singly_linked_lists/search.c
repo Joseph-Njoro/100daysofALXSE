affc1db8c3eb7d12a8028a291edf760fa1a06959
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-24 23:26:48 +0300

    fix

--- singly_linked_lists/search.c
+++ singly_linked_lists/search.c
@@ -53,7 +53,7 @@ int main() {
     int searchValue = 2;
     Node* foundNode = searchNode(head, searchValue);
     if (foundNode != NULL) {
-        printf("Node with value d found at address %p\n", searchValue, (void*)foundNode);
+        printf("Node with value %d found at address %p\n", searchValue, (void*)foundNode);
     } else {
         printf("Node with value %d not found in the list\n", searchValue);
     }
