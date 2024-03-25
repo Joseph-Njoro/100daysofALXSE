dcfda33b791d86536c2b91f7117b474ce9d4221d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:13:20 +0300

    fix

--- bit_manipulation/right_shift.c
+++ bit_manipulation/right_shift.c
@@ -4,7 +4,7 @@ int main() {
     int a = 5; // Binary: 0101
 
     int result = a >> 1; // Right shift by 1
-    printf(f"Result of %d >> 1 = %d\n", a, result);
+    printf("Result of %d >> 1 = %d\n", a, result);
 
     return 0;
 }
