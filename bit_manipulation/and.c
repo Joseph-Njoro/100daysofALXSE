d32807ff689b7d31b9d9737b8379c181eed25cd0
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:10:02 +0300

    and

---
+++ bit_manipulation/and.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5; // Binary: 0101
+    int b = 3; // Binary: 0011
+
+    int result = a & b; // Bitwise AND
+    printf("Result of %d & %d = %d\n" a, b, result);
+
+    return 0;
+}
