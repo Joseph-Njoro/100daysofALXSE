9fc7f89eb59e58843bf964554df7f6dc7f297327
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:16:33 +0300

    is nth bit set?

---
+++ bit_manipulation/set_bit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num = 5; // Binary: 0101
+
+    int n = 2; // Bit to check (0-based index)
+    int iSSet = (num >> n) & 1; // Check if nth bit is set
+
+    printf("Bit %d is %s\n", n, isSet ? "Set" : "Not set");
+
+    return 0;
+}
