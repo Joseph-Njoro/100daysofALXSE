662e39d4b4d3179b80c12646f447fd0365325047
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:14:37 +0300

    check if no is even or odd

---
+++ bit_manipulation/even_or_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 6; // Example number
+
+    if (num & 1) {
+        printf(%d is Odd\n", num);
+    } else {
+        printf("%d is Even\n", num);
+    }
+
+    return 0;
+}
