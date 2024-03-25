c5301bbd96371aa1ad2df774dbb1aa25be8e5273
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:07:11 +0300

    fix

--- bit_manipulation/basic.c
+++ bit_manipulation/basic.c
@@ -5,7 +5,7 @@ int main() {
     int b = 3; // Binary: 0011
 
     printf("a & b = %d\n", a & b); // Bitwise AND
-    printf("a | b = %d\n", a | b) // Bitwise OR
+    printf("a | b = %d\n", a | b) ;// Bitwise OR
     printf("a ^ b = %d\n" a ^ b); // Bitwise XOR
     printf(" ` a = %d\n", ~a);       // Bitwise NOT
     printf("a < 2 = %d\n", a << 2); // Left shift
