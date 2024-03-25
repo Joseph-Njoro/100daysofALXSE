f94bfee1cf670be9de03463304179bb3e39130cf
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:06:34 +0300

    bit manipulation

---
+++ bit_manipulation/basic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5; // Binary: 0101
+    int b = 3; // Binary: 0011
+
+    printf('a & b = %d\n", a & b); // Bitwise AND
+    printf("a | b = %d\n", a | b) // Bitwise OR
+    printf("a ^ b = %d\n" a ^ b); // Bitwise XOR
+    printf(" ` a = %d\n", ~a);       // Bitwise NOT
+    printf("a < 2 = %d\n", a << 2); // Left shift
+    printf("a > 1 = %d\n", a >> 1); // Right shift
+
+    return 0;
+}
