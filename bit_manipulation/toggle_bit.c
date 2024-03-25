b37608fb72656865761d4a20341efe6db4b54d1a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-25 23:15:59 +0300

    fix

--- bit_manipulation/toggle_bit.c
+++ bit_manipulation/toggle_bit.c
@@ -4,7 +4,7 @@ int main() {
     int num = 5; // Binary: 0101
 
     int n = 2; // Bit to toggle (0-based index)
-    int result = num ^ (1 << n) // Toggle nth bit
+    int result = num ^ (1 << n); // Toggle nth bit
 
     printf("Result after toggling %dth bit: %d\n", n, result);
 
