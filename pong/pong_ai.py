09787985c0afb1f0a64893ff564daaf7bcc29ee4
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-27 23:25:03 +0300

    fix

--- pong/pong_ai.py
+++ pong/pong_ai.py
@@ -73,7 +73,7 @@ clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type = pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
     
     # Move player paddle
