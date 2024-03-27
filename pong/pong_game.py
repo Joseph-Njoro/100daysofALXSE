92f80342008516d33477917713dcf82862b140d1
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-27 23:13:46 +0300

    fix

--- pong/pong_game.py
+++ pong/pong_game.py
@@ -14,7 +14,7 @@ pygame.display.set_caption("Pong")
 running = True
 while running:
     for event in pygame.event.get():
-        if event,type = = pygame.QUIT:
+        if event,type == pygame.QUIT:
             running = False
         
     window.fill((0, 0, 0))  # Clear the screen
