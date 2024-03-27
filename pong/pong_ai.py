76a98f0c4c6aa34fd40fcc0aba39a07b47d86eca
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-27 23:53:28 +0300

    display window altered

--- pong/pong_ai.py
+++ pong/pong_ai.py
@@ -5,8 +5,8 @@ import random
 pygame.init()
 
 # Set up the game window
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 800
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Pong")
 
