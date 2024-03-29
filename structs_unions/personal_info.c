cac75366a5b7a9a511fd52af10138cb1afa0c414
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:30:51 +0300

    personal info

---
+++ structs_unions/personal_info.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Person {
+    char name[50];
+    int age;
+    union Contact {
+        char email[50];
+        char phone[15];
+    } contact;
+};
+
+int main() {
+    struct Person person1 = {"John", 25, {email = "john@example.com"}};
+    printf("Name: %\nAge: %dEmail: %s\n", person1.name, person1.age person1.contact.email);
+    return 0;
+}
