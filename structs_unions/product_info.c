88b27ef8c6e5bfbe10ca07c7a563651c41f11ee7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:38:22 +0300

    fix

--- structs_unions/product_info.c
+++ structs_unions/product_info.c
@@ -13,4 +13,8 @@ int main() {
     struct Product product1 = {"Laptop", 1001, {.unit_price = 899.99}};
     struct Product product2 = {"Smartphone", 1002, {.bulk_price = 499.99}};
 
-    printf("Product 1: Name = %s, Product ID = %
+    printf("Product 1: Name = %s, Product ID = %d, Unit Price = %.2f\n", product1.name, product1.product_id, product1.price.unit_price);
+    printf("Product 2: Name = %s, Product ID = %d, Bulk Price = %.2f\n", product2.name, product2.product_id, product2.price.bulk_price);
+
+    return 0;
+}
