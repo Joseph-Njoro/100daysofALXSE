c6eaf33dd8b89600dd4db744e9f91212ccb20e46
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:36:50 +0300

    fix

--- structs_unions/account_info.c
+++ structs_unions/account_info.c
@@ -13,7 +13,7 @@ int main() {
     struct Account checking = {"John Doe", 1001, {.current_balance = 2500}};
     struct Account savings = {"Jane Smith", 1002, {.savings_balance = 5000}};
 
-    printf("Checking Account: Holder = s, Account Number = %d, Current Balance = %.2f\n", checking.account_holder, checking.account_number, checking.balance.current_balance);
+    printf("Checking Account: Holder = %s, Account Number = %d, Current Balance = %.2f\n", checking.account_holder, checking.account_number, checking.balance.current_balance);
     printf("Savings Account: Holder = %s, Account Number = %d, Savings Balance = %.2f\n", savings.account_holder, savings.account_number, savings.balance.savings_balance);
 
     return 0;
