73aafaaac2320d14b7eda0f0aba5c20f54ad1748
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:22:58 +0300

    struct basics

--- structs_unions/struct.c
+++ structs_unions/struct.c
@@ -11,7 +11,7 @@ int main() {
     p1.x = 10;
     p1.y = 20;
 
-    printf("Point coordinates: (%d %d)\n", p1.x, p1.y);
+    printf("Point coordinates: (%d, %d)\n", p1.x, p1.y);
 
     return 0;
 }
