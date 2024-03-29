cf2288143729f49ff538c0ed00404ba7885d2c8f
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:22:26 +0300

    fix

---
+++ structs_unions/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main() {
+    struct Point p1;
+
+    p1.x = 10;
+    p1.y = 20;
+
+    printf("Point coordinates: (%d %d)\n", p1.x, p1y);
+
+    return 0;
+}
