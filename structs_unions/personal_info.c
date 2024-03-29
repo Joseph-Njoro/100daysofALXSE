ce70e5fb94e6269eedd8eb6892b92f61bd33d10c
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:31:06 +0300

    fix

--- structs_unions/personal_info.c
+++ structs_unions/personal_info.c
@@ -10,7 +10,7 @@ struct Person {
 };
 
 int main() {
-    struct Person person1 = {"John", 25, {email = "john@example.com"}};
+    struct Person person1 = {"John", 25, {.email = "john@example.com"}};
     printf("Name: %\nAge: %dEmail: %s\n", person1.name, person1.age, person1.contact.email);
     return 0;
 }
