010fe630eff795c2e639984af1018fbb900adf16
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-29 23:32:42 +0300

    fix

--- structs_unions/shape_info.c
+++ structs_unions/shape_info.c
@@ -15,12 +15,16 @@ struct Shape {
             float height;
         } triangle;
     } properties;
-};
+}
 
 int main() {
     struct Shape circle = {0, {.circle.radius = 5}};
     struct Shape rectangle = {1, {.rectangle.width = 4, .rectangle.height = 3}};
     struct Shape triangle = {2, {.triangle.base = 3, .triangle.height = 4}};
 
-    printf("Circle: Radius = %.2f\n", circle.properties.circle.radius);
-    printf("Rectangle: Width = %.2f, Height = %.2f\n", rectangle.properties.rectangle.width, rectangle.properties.rectangle.height
+    printf("Circle: Radius = 2f\n", circle.properties.circle.radius);
+    printf("Rectangle: Width = %.2f, Height = %.2f\n", rectangle.properties.rectangle.width, rectangle.properties.rectangle.height);
+    printf("Triangle: Base = %.2f, Height = %2f\n", triangle.properties.triangle.base, triangle.properties.triangle.height);
+
+    return 0;
+}
