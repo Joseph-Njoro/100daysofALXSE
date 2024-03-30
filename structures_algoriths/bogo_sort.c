ad850f3e9ad82abd212adc2f1a13240c9cfea1c2
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-30 23:23:30 +0300

    bogo sort

---
+++ structures_algoriths/bogo_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int isSorted(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void shuffle(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int j = rand() % n;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+void bogoSort(int arr[], int n) {
+    while (!isSorted(arr, n)) {
+        shuffle(arr, n);
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    srand(time(NULL)); // Seed the random number generator
+    bogoSort(arr, n);
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
