ef33384f725f29e9e4c769c823b9e929351fa79a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-03-30 23:37:00 +0300

    lcm calcultion

---
+++ structures_algoriths/lcm.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int num1 = 4, num2 = 6;
+    printf("LCM of %d and %d is %d\n", num1, num2, lcm(num1, num2));
+    return 0;
+}
