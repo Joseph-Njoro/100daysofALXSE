8bb637a3e22507bee828e06f4ecf2d677227d1d2
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:21:46 +0300

    fix

--- argc_argv/javascript/dtat_types.js
+++ argc_argv/javascript/dtat_types.js
@@ -2,7 +2,7 @@
 let num = 10;
 let message = "Hello, world!";
 let is Student = true;
-let myArray = [1 2, 3, 4, 5];
+let myArray = [1, 2, 3, 4, 5];
 lt person = { name: "Joseph", age: 25 };
 
 // Print data types
