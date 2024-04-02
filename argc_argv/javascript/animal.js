edbd162101d3a6af613e5bc79266d37254812588
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:31:32 +0300

    fix

--- argc_argv/javascript/animal.js
+++ argc_argv/javascript/animal.js
@@ -1,52 +1,52 @@
 class Animal {
-    constructor(name, species) {
-      this.name = name;
-      this.species = species;
-    }
-  
-    introduce() {
-      console.log(`Hi, I'm ${this.name}, a ${this.species}.`);
-    }
+  constructor(name, species) {
+    this.name = name;
+    this.species = species;
   }
-  
-  class Dog extends Animal {
-    constructor(name, breed, age) {
-      super(name, "dog");
-      this.breed = breed;
-      this.age = age;
-    }
-  
-    bark() {
-      console.log("Woof woof!");
-    }
-  
-    introduce() {
-      super.introduce();
-      console.log(`I'm a ${this.age}-year-old ${this.breed}.`);
-    }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.species}.`);
   }
-  
-  class Cat extends Animal {
-    constructor(name, color) {
-      super(name, "cat");
-      this.color = color;
-    }
-  
-    meow() {
-      console.log("Meow meow!");
-    }
-  
-    introduce() {
-      super.introduce();
-      console.log(`I'm ${this.color} in color.`);
-    }
+}
+
+class Dog extends Animal {
+  constructor(name, breed, age) {
+    super(name, "dog");
+    this.breed = breed;
+    this.age = age;
   }
-  
-  const dog = new Dog("Buddy", "Golden Retriever", 5);
-  const cat = new Cat("Whiskers", "Tabby");
-  
-  dog.introduce();
-  dog.bark();
-  
-  cat.introduce();
-  cat.meow();  
+
+  bark() {
+    console.log("Woof woof!");
+  }
+
+  introduce() {
+    super.introduce();
+    console.log(`I'm a ${this.age}-year-old ${this.breed}.`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, color) {
+    super(name, "cat");
+    this.color = color;
+  }
+
+  meow() {
+    console.log("Meow meow!");
+  }
+
+  introduce() {
+    super.introduce();
+    console.log(`I'm ${this.color} in color.`);
+  }
+}
+
+const dog = new Dog("Buddy", "Golden Retriever", 5);
+const cat = new Cat("Whiskers", "Tabby");
+
+dog.introduce();
+dog.bark();
+
+cat.introduce();
+cat.meow();
