bfed5955a14122590bf31c31181e772667590b37
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:19:15 +0300

    fix

--- argc_argv/javascript/variables.js
+++ argc_argv/javascript/variables.js
@@ -1,9 +1,9 @@
 // Declare variables
 var age = 25;
-let name = "Joseph';
+let name = "Joseph";
 const PI = 3.14;
 
 // Print variables
-consolelog("Age:" age);
+console.log("Age:", age);
 console.log("Name:", name);
 console.log("PI:", PI)
