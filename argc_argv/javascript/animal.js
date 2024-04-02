de0c759e7f3f25cee5e4dc9b6c1f3396fdffb072
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:30:27 +0300

    animals

---
+++ argc_argv/javascript/animal.js
@@ -0,0 +1,52 @@
+class Animal {
+    constructor(name, species) {
+      this.name = name;
+      this.species = species;
+    }
+  
+    introduce() {
+      console.lo(`Hi, I'm ${this.name}, a ${this.species}.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed, age) {
+      super(name, "dog");
+      this.breed = breed;
+      this.age = age;
+    }
+  
+    bark() {
+      console.log("Woof woof!");
+    }
+  
+    introduce() {
+      super.introduce();
+      console.log(I'm a ${this.age}-year-old ${this.breed}.`);
+    }
+  }
+  
+  class at extends Animal {
+    constructor(name, color) {
+      super(name, "cat");
+      thiscolor = color;
+    }
+  
+    meow() {
+      console.log("Meow meow!");
+    }
+  
+    introduce() {
+      super.introduce(;
+      console.log(`I'm {this.color} in color.`);
+    }
+  }
+  
+  const dog = new Dog("Buddy", "Golden Retriever" 5);
+  const cat = new Cat("Whiskers", "Tabby");
+  
+  dog.introduce();
+  dogbark();
+  
+  cat.introduce();
+  cat.meow()  
