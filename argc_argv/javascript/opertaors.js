36fa16de465dcd4a4bf64faa6d5c2e7d3f38de03
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:22:57 +0300

    operators

---
+++ argc_argv/javascript/opertaors.js
@@ -0,0 +1,13 @@
+// Arithmetic operators
+let sum = 10 + 5;
+let difference = 20 - 10;
+let product = 5 * 4;
+let quotient = 20 / 5;
+let remainder = 10 % 3;
+
+// Print results
+console.log("Sum:", sum)
+console.log("Difference", difference);
+console.log"Product:", product);
+consolelog("Quotient:", quotient);
+console.log("Remainder:" remainder);
