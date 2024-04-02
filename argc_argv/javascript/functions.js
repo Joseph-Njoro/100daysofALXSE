a9d027858dec1cb05d7887dc33cc9a2db6f440e8
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:24:50 +0300

    functions

---
+++ argc_argv/javascript/functions.js
@@ -0,0 +1,10 @@
+/*
+Function declaration 
+Function call
+**/
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+let greeting = greet("Joseph");
+console.log(greeting); // Output: Hello, Joseph!
