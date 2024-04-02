c4441b850e815fd5fa8482ec2fde7b484ce53841
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:22:06 +0300

    fix

--- argc_argv/javascript/dtat_types.js
+++ argc_argv/javascript/dtat_types.js
@@ -8,6 +8,6 @@ let person = { name: "Joseph", age: 25 };
 // Print data types
 console.log("Number:", num);
 console.log("String:", message);
-console.log("Boolean:" isStudent);
+console.log("Boolean:", isStudent);
 console.log("Array", myArray);
 console.log("Object:", person)
