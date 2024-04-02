e4090a35b936068bc57e3182636779b6fcfe637d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:51:23 +0300

    conditional if statement

---
+++ argc_argv/javascript/condition_if.js
@@ -0,0 +1,9 @@
+let temperature = 20;
+
+if (temperature < 0) {
+    console.log("It's freezing!");
+} else if (temperature >= 0 && temperature <= 20) {
+    console.log("It's chilly.");
+} else {
+    console.log("It's warm outside!");
+}
