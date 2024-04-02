eff4d0914fe1874a07d93f16720145ca2bbb1250
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-02 23:21:23 +0300

    data types

---
+++ argc_argv/javascript/dtat_types.js
@@ -0,0 +1,13 @@
+// Primitive data types
+let num = 10;
+let message = "Hello, world!";
+let is Student = true;
+let myArray = [1 2, 3, 4, 5];
+lt person = { name: "J osep h", age: 25 ;
+
+// Print data types
+console.log("Number:", num);
+consolelog("String:", message);
+console.log("Boolean:" isStudent);
+console.log("Array", myArray);
+console.log("Object:", person)
