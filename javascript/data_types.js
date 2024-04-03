83e86b11f7454777b78b855c74adf922bfa3ea55
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-03 23:34:15 +0300

    data types

---
+++ javascript/data_types.js
@@ -0,0 +1,15 @@
+let message = "Hello, World!";
+let age = 30
+let isStudent = false;
+let student = {
+  name: "John Doe"
+  age: 30,
+  isStudent true
+}
+let numbers =+ [1 2, 3, 4, 5];
+
+conole.log(message);
+console.lg(age);
+console.log(isStudent);
+consolelog(student);
+console.log(numbers)
