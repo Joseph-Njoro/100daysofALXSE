147637d44cd900765f2705f392a650e3e4594323
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-03 23:42:57 +0300

    fix

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,9 +1,9 @@
 let student = {
-    name: "John Doe",
-    age: 30,
-    isStudent: true
-  };
-  
-  console.log(student.name); // John Doe
-  console.log(student.age); // 30
-  console.log(student.isStudent); // true
+  name: "John Doe",
+  age: 30,
+  isStudent: true
+};
+
+console.log(student.name); // John Doe
+console.log(student.age); // 30
+console.log(student.isStudent); // true
