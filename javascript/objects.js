a4ad4380a72bbdd557bd03310bade81783ffe803
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-03 23:41:46 +0300

    objects

---
+++ javascript/objects.js
@@ -0,0 +1,8 @@
+let student = {
+    name: "John Doe", age: 30,
+    isStudent: tru
+  }
+  
+  console.lo(student.name); // John Doe
+  console.log(student.ge); // 30
+  console.log(student.isStdent); // true
