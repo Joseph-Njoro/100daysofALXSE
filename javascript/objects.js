3a9c83f27fd569be0e9913f34a08f0ebb2d70def
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-03 23:42:35 +0300

    fix

--- javascript/objects.js
+++ javascript/objects.js
@@ -4,6 +4,6 @@ let student = {
     isStudent: true
   };
   
-  console.lo(student.name); // John Doe
+  console.log(student.name); // John Doe
   console.log(student.ge); // 30
   console.log(student.isStdent); // true
