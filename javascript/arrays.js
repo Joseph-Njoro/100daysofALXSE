1ff576350d492f82190b59c63139ef57d6380f97
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-03 23:41:06 +0300

    foden 61'

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -2,6 +2,6 @@ let numbers = [1, 2, 3, 4, 5];
 
 console.log(numbers[0]); 
 console.log(numbers[1]); 
-console.og(numbers[2]); 
+console.log(numbers[2]); 
 console.log(numbers[3]); 
 console.log(numbers[4]); 
