b081a769aa7d0fc389c4b7d4b2719ed6a5d127f5
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-05 23:47:25 +0300

    fix

--- Utility_Cleaner.py
+++ Utility_Cleaner.py
@@ -9,7 +9,7 @@ def renam_files(files)
     renamed_files = [
     for file n files:
         #Get the filename and extension
-        name ext = ospath.splitext(file)
+        name ext = os.path.splitext(file)
         
         # Replace spaces with underscores and remove non-alphabetic characters in the filename
         new_name = ''.join(char if char.isalpha() or char.isspace() else '_' for char in name)
