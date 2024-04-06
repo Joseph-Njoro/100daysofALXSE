7fc3587597e29898420b8e8e8c50d4fc711e21b7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-06 22:32:26 +0300

    fix

--- javascript/Tic_Tac_Toe.js
+++ javascript/Tic_Tac_Toe.js
@@ -79,6 +79,6 @@ function resetGame() {
 // Example usage:
 playerMove(0, 0); // Player X moves to cell (0, 0)
 playerMove(1, 1); // Player O moves to cell (1, 1)
-playerMove0, 1); // Player X moves to cell (0, 1)
+playerMove(0, 1); // Player X moves to cell (0, 1)
 playerMove(1 0); // Player O moves to cell (1, 0)
 playerMove(0, 2) // Player X moves to cell (0, 2)
