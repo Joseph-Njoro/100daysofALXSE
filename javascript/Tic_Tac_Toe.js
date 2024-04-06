e5ed7e6c44414ef26c1865b4c0b6c2e1bbfff3a2
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-06 22:36:45 +0300

    fix

--- javascript/Tic_Tac_Toe.js
+++ javascript/Tic_Tac_Toe.js
@@ -66,7 +66,103 @@ function playerMove(row, col) {
   }
 }
 
-// Function to reset the game
+// Function to reset the gameaconst readline = require('readline');
+
+class TicTacToe {
+    constructor() {
+        this.board = [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ];
+        this.currentPlayer = 'X';
+        this.movesLeft = 9;
+        this.gameOver = false;
+    }
+
+    printBoard() {
+        console.log('  1 2 3');
+        for (let i = 0; i < 3; i++) {
+            console.log(`${i + 1} ${this.board[i].join(' ')}`);
+        }
+    }
+
+    isValidMove(row, col) {
+        return this.board[row][col] === '-';
+    }
+
+    makeMove(row, col) {
+        if (this.isValidMove(row, col)) {
+            this.board[row][col] = this.currentPlayer;
+            this.movesLeft--;
+            this.checkWinner(row, col);
+            this.switchPlayer();
+        } else {
+            console.log('Invalid move. Try again.');
+        }
+    }
+
+    switchPlayer() {
+        this.currentPlayer = (this.currentPlayer === 'X') ? 'O' : 'X';
+    }
+
+    checkWinner(row, col) {
+        if (this.checkRow(row) || this.checkColumn(col) || this.checkDiagonal(row, col)) {
+            this.gameOver = true;
+            console.log(`Player ${this.currentPlayer} wins!`);
+        } else if (this.movesLeft === 0) {
+            this.gameOver = true;
+            console.log('It\'s a draw!');
+        }
+    }
+
+    checkRow(row) {
+        return (this.board[row][0] === this.currentPlayer &&
+                this.board[row][1] === this.currentPlayer &&
+                this.board[row][2] === this.currentPlayer);
+    }
+
+    checkColumn(col) {
+        return (this.board[0][col] === this.currentPlayer &&
+                this.board[1][col] === this.currentPlayer &&
+                this.board[2][col] === this.currentPlayer);
+    }
+
+    checkDiagonal(row, col) {
+        if (row === col || row + col === 2) {
+            return ((this.board[0][0] === this.currentPlayer && this.board[1][1] === this.currentPlayer && this.board[2][2] === this.currentPlayer) ||
+                    (this.board[0][2] === this.currentPlayer && this.board[1][1] === this.currentPlayer && this.board[2][0] === this.currentPlayer));
+        }
+        return false;
+    }
+}
+
+function main() {
+    const ticTacToe = new TicTacToe();
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.log('Welcome to Tic Tac Toe!\n');
+    ticTacToe.printBoard();
+
+    rl.on('line', (input) => {
+        const [row, col] = input.trim().split(' ');
+        if (!ticTacToe.gameOver && ticTacToe.isValidMove(row - 1, col - 1)) {
+            ticTacToe.makeMove(row - 1, col - 1);
+            ticTacToe.printBoard();
+        } else if (!ticTacToe.gameOver) {
+            console.log('Invalid move. Try again.');
+        }
+        if (ticTacToe.gameOver) {
+            rl.close();
+        }
+    });
+}
+
+main();
+
 function resetGame() {
   board = [
     ['', '', ''],
