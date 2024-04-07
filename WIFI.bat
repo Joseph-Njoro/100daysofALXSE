0a3981f7381da7e8b034d2124577d2424392e761
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-08 00:05:22 +0300

    wifi

--- WIFI.bat
+++ WIFI.bat
@@ -14,17 +14,16 @@ rem Prompt user to input WiFi interface name
 set /p interface="Enter WiFi interface name: "
 
 rem Get password for the specified WiFi interface
-netsh wlan show profile name="%interface%" key=clear | findstr /C:"Key Content" > "%interface%_password.txt"
+for /f "tokens=2 delims=: " %%b in ('netsh wlan show profile name="%interface%" key=clear ^| findstr /c:"Key Content"') do (
+    set password=%%b
+)
 
 rem Check if password retrieval was successful
-if %errorlevel% NEQ 0 (
+if "%password%"=="" (
     echo Error: Unable to retrieve the password for %interface%.
 ) else (
+    echo %password% > "%interface%_password.txt"
     echo Password for WiFi interface %interface% has been saved to %interface%_password.txt.
 )
 
-rem Clean up temporary files
-del interfaces.txt
-
 endlocal
-
