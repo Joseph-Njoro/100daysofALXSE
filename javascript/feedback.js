865c92bd1822dcae2047153f3f003d2f1f4a3ea8
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-07 23:14:23 +0300

    fix

--- javascript/feedback.js
+++ javascript/feedback.js
@@ -37,7 +37,7 @@ cells.forEach(cell => {
 });
 
 // Add a text indicating which player is playing
-ons currentPlayerText = document.createElement('p');
+onst currentPlayerText = document.createElement('p');
 urentlayrTetid = currentplayer
 currentPlayerText.textContent = `Current player currentPlayer}
 document.body.appendChild(currentPlayerText)
