91e1807c9bbc9e043eb893272325e0fc043e6cec
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-08 23:25:17 +0300

    fix

--- python_wifi_code.py
+++ python_wifi_code.py
@@ -422,7 +422,7 @@ def pixieS():
     global WPSpin
     fout2 = open("fPixiewpsOut", "w")
     ferr2 = open("fPixiewpsErrors", "w")
-    runpixie = Popen(["ixiewps", "-e", PKE, "-s", EHash1, "-z", EHash2, "-a", AuthKey, "-S"], stdout=fout2, stderr=ferr2)
+    runpixie = Popen(["pixiewps", "-e", PKE, "-s", EHash1, "-z", EHash2, "-a", AuthKey, "-S"], stdout=fout2, stderr=ferr2)
     Popen.wait(runpixie)
     f = open"fPixiewpsOut")
     line = f.eadline()
