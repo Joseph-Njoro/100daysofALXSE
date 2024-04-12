4fbbe1d67e635a9c659b863498ee2429909ad10f
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-12 22:18:51 +0300

    f

--- music.py
+++ music.py
@@ -16,7 +16,7 @@ class MusicPlayer:
         self.initialize_player()
         self.initialize_visualizer()
 
-    def initialize_player(sel):
+    def initialize_player(self):
         self.play_button = tk.utton(self.root, text="Play", command=self.play_music)
         self.play_button.pack()
         self.pause_button = tk.utton(self.root, text="Pause", command=self.pause_music)
