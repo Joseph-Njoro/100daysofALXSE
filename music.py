8bbe31132c7eba958792ab12e2714d8915d23786
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-12 22:21:41 +0300

    f

--- music.py
+++ music.py
@@ -53,7 +53,7 @@ class MusicPlayer:
 
     def add_music(self):
         file_path = filedialog.askopenfilename(initialdir=self.current_dir, title="Select Music", filetypes=(("Audio Files", "*.mp3"), ("All Files", "*.*")))
-        if file_path
+        if file_path:
             self.playlist.append(file_path
             self.playlist_box.insert(tk.END, os.path.basename(file_path))
     def run_visualizer(sef):
