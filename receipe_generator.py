73259b5b17210ad73f6ccd1ff466acacf4d4165d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-13 23:41:14 +0300

    fix

--- receipe_generator.py
+++ receipe_generator.py
@@ -2,7 +2,7 @@ import rando
 # Define lits of ingredients and recipes
 ingredients 
     'vegetables': ['carrots', 'broccoli', 'spinach', 'bell peppers', 'onions','tomatoes', 'zucchini', 'mushrooms'],
-    'protein': ['chicken', 'beef', 'tofu', 'salmon', 'shrimp', 'eggsbeans']
+    'protein': ['chicken', 'beef', 'tofu', 'salmon', 'shrimp', 'eggs', 'beans']
     'carbs': ['pasta', 'rice', 'quinoa', 'potatoes', 'bread', 'couscous', 'tortillas'],    'spices': ['salt', 'pepper', 'garlic powder', 'paprika', 'cumin', 'oregano', 'curry powder', 'chili flakes'],
    'sauces': ['soy sauce', 'tomato sauce', 'pesto', 'barbecue sauce', 'sriracha', 'honey mustard', 'vinaigrette']
 }
