bedb83979aa65ecaa92a5485d1d014a49190b99e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-13 23:41:40 +0300

    s

--- receipe_generator.py
+++ receipe_generator.py
@@ -4,7 +4,8 @@ import random
 ingredients = {
     'vegetables': ['carrots', 'broccoli', 'spinach', 'bell peppers', 'onions', 'tomatoes', 'zucchini', 'mushrooms'],
     'protein': ['chicken', 'beef', 'tofu', 'salmon', 'shrimp', 'eggs', 'beans'],
-    'carbs': ['pasta', 'rice', 'quinoa', 'potatoes', 'bread', 'couscous', 'tortillas'],    'spices': ['salt', 'pepper', 'garlic powder', 'paprika', 'cumin', 'oregano', 'curry powder', 'chili flakes'],
+    'carbs': ['pasta', 'rice', 'quinoa', 'potatoes', 'bread', 'couscous', 'tortillas'],
+    'spices': ['salt', 'pepper', 'garlic powder', 'paprika', 'cumin', 'oregano', 'curry powder', 'chili flakes'],
    'sauces': ['soy sauce', 'tomato sauce', 'pesto', 'barbecue sauce', 'sriracha', 'honey mustard', 'vinaigrette']
 }
 recipes = 
