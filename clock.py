a7a4db76b57bbde2b0c2844b9c3b11f87c190f94
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-14 23:09:09 +0300

    fix

--- clock.py
+++ clock.py
@@ -28,6 +28,7 @@ class AnalogClockApp:
         self.timer_running = False
         self.timer_seconds = 0
         self.timer_duration = 0
+
         self.update_clock)
         self.update_stopwach()
         self.update_timer()
