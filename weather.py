9e4521cfe3954c93148f520a2c04e38f128d5a1f
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-15 20:32:12 +0300

    weather app

--- weather.py
+++ weather.py
@@ -28,7 +28,7 @@ lass WeatherApp
        weather_info = f"City: {city_name}\nWeather: {weather_description}\nTemperature: {temperature}°C\nHumidity: {humidity}%\nWind Speed: {wind_speed} m/s"
        self.weather_info_label.config(text=weather_info)
        else
-           self.eather_info_label.config(text="City not found")
+           self.weather_info_label.config(text="City not found")
 f __name__ == __ain__":
    root = tk.Tk)   app = Weatherp(root)
    root.mainloop(
