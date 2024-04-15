014e565e9bd7e5350ca14ac64a3055f806271f6a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-15 20:37:50 +0300

    wetaher

--- weather.py
+++ weather.py
@@ -34,7 +34,7 @@ class WeatherApp:
             city_name = data["name"]
 
             weather_info = f"City: {city_name}\nWeather: {weather_description}\nTemperature: {temperature}°C\nHumidity: {humidity}%\nWind Speed: {wind_speed} m/s"
-        self.weather_info_label.config(text=weather_info)
+            self.weather_info_label.config(text=weather_info)
         else
             self.weather_info_label.config(text="City not found")
 if __name__ == __main__":
