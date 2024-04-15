05dfdc2a3b29747b1b6db7987ff7255e097a9d3d
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-15 20:37:04 +0300

    wetager

--- weather.py
+++ weather.py
@@ -25,6 +25,7 @@ class WeatherApp:
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
         response = requests.get(url)
         data = response.json()
+
         if data["cod"] == 200
             weather_description  data["weather"][0]["description"].capitalize()
             temperature = data["min"]["temp"]
