faabe56155a857fe2afc1761dd9b1c24af36099a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-16 23:05:54 +0300

    dsa

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ mport requests
 lass WeatherApp
    def __init__(self,root):
        self.root = roo
-       self.root.title("Weather App)
+        self.root.title("Weather App)
         self.root.geometry("400x200")
         self.city_label = tk.Label(root, text="Enter City:"
         self.city_label.grid(row=0, column=0, padx=10, pady=10)
