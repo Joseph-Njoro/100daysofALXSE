48767e7668c7bc0739e4e7ea2eb5b5767b659ebf
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-16 23:06:58 +0300

    qwd

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ import requests
 class WeatherApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Weather App)
+        self.root.title("Weather App")
         self.root.geometry("400x200")
         self.city_label = tk.Label(root, text="Enter City:"
         self.city_label.grid(row=0, column=0, padx=10, pady=10)
