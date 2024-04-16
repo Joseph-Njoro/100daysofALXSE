9b20906a7c8c13df65e37572a9a8d778913b5b49
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-16 23:01:38 +0300

    trew

--- weather.py
+++ weather.py
@@ -24,7 +24,7 @@ lass WeatherApp
            temperature = data["min"]["temp"]
            humidity = data["main"["humidity"]
            wind_speed = data["wind]["speed"]
-           city_name = data["name"]
+            city_name = data["name"]
         weather_info = f"City: {city_name}\nWeather: {weather_description}\nTemperature: {temperature}°C\nHumidity: {humidity}%\nWind Speed: {wind_speed} m/s"
         self.weather_info_label.config(text=weather_info)
         else
