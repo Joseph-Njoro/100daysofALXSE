e2ba09f1d036b3c87facbbf699e4ddce3e7e46ca
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:18:41 +0300

    server

--- Task_Tracker/server/server.js
+++ Task_Tracker/server/server.js
@@ -1,5 +1,5 @@
 / server/server.j
-onst app = require('./app');
+const app = require('./app');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
   console.log(`Server is running on port ${PORT}`)
