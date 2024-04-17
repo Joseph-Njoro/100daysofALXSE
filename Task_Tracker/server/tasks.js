d484343e57e16b14142472f41606fc970cc62ac2
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:21:01 +0300

    tasks

---
+++ Task_Tracker/server/tasks.js
@@ -0,0 +1,25 @@
+/ server/routes/tasks.j
+onst express = require('expres');
+onst router = express.Router();
+onst Task = require('../models/Task');
+/ Gt all tasks
+oute.get('/', async (req, res) => {
+ try 
+   const tasks = aait Task.find();
+   res.json(tasks);  } catch (err) {
+ res.status(500).json({ message: err.message });
+ 
+);
+/ Create a new tas
+outer.post('/', async (eq, res) => {
+ const task = new Task({    title: req.body.title,
+escription: req.body.description,
+   / Add more task properties as needed
+ });
+ try 
+   const newTask = await task.sae();
+   res.status(201).json(newTask);  } catch (err) {
+ res.status(400).json({ message: err.message });
+ 
+);
+module.exports = router
