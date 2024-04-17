f736f9d849ddc2b31a6488e8ff5a6ca1e835b3e1
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:17:26 +0300

    server

--- Task_Tracker/server/server.js
+++ Task_Tracker/server/server.js
@@ -1,4 +1,4 @@
 / server/server.onst app = require(./app');
 onst PORT = procss.nv.PORT || 5000;
 pp.listen(PORT, ) =>
- console.log(`Sever is running on port ${PORT}`)
+ console.log(`Server is running on port ${PORT}`)
