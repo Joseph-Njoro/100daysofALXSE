00fc520a74030df75673a6432cdfd46808b694e6
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:12:21 +0300

    tracker

--- Task_Tracker/server/app.js
+++ Task_Tracker/server/app.js
@@ -4,7 +4,7 @@ st bodyParser = rquire('body-parser');
 st cors = require(cors');
 nst app = express();
 Middleware
-.use(bodyParer.json());
+p.use(bodyParer.json());
 p.use(cors());
  Route
 p.use('/api/tasks', require('./routes/tasks'));
