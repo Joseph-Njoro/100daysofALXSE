e2ffe0f94eb77078e9e23258eaf9c52a41ef54b4
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:13:56 +0300

    fix

--- Task_Tracker/server/app.js
+++ Task_Tracker/server/app.js
@@ -5,7 +5,7 @@ onst cors = require(cors');
 const app = express();
 / Middleware
 pp.use(bodyParer.json());
-p.use(cors());
+pp.use(cors());
  Route
 p.use('/api/tasks', require('./routes/tasks'));
 odule.exports = app
