d900fb6edb49664d3119d6d3a3f6241e51b96681
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-17 23:15:39 +0300

    fixfix

--- Task_Tracker/server/app.js
+++ Task_Tracker/server/app.js
@@ -1,7 +1,7 @@
 // server/app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require(cors');
+const cors = require('cors');
 
 const app = express();
 // Middleware
