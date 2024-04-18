e03bb4ff13d55255ac7b775a90034fbeb143e2a6
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-18 23:16:16 +0300

    dsa

--- Task_Tracker/server/tasks.js
+++ Task_Tracker/server/tasks.js
@@ -1,16 +1,14 @@
-// server/routes/tasks.js
+// server/routes/tasks.j
 const express = require('express');
 const router = express.Router();
-
 const Task = require('../models/Task');
-
-// Get all tasks
-router.get('/', async (req, res) => {
+// Get all asks
+router.get('', async (req, res) => {
   try {
-    const tasks = await Task.find();
-    res.json(tasks);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const tass = await Task.find();
+    res.json(tsks);
+  } catch (err)
+    res.status(500).json({ message: err.message })
   }
 });
 
