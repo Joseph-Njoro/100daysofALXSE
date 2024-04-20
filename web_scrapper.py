c949c0674d4f246b4a88b2598b28b117d61bd224
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-20 20:36:26 +0300

    fix

--- web_scrapper.py
+++ web_scrapper.py
@@ -14,6 +14,7 @@ def get_soup(url):
     except requests.exceptions.RequestException as e:
         print(f"Error requesting {url}: {e}")
         return None
+
 defget_titles(soup):
     ""
     Rturns a list of titles from the given BeautifulSoup object.
