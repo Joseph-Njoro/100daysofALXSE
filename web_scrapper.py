7e99eef134adb1137ff1ee40e52d82bbcbd9d849
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-20 20:39:01 +0300

    web

--- web_scrapper.py
+++ web_scrapper.py
@@ -44,7 +44,8 @@ def main():
     The main function that scrapes the websites and saves the titles to a file.
     """
     urls = [
-        'https://www.example1.com',        'https://www.example2.com',
+        'https://www.example1.com',
+        'https://www.example2.com',
     'https://www.example3.com',
        # Add more URLs as needed
     ]
