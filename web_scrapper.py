f0fb52e5ad7237371e9f0d7073fbca14b537852e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-21 23:15:22 +0300

    fix

--- web_scrapper.py
+++ web_scrapper.py
@@ -61,7 +61,7 @@ def main():
             else:
                 print("No titles found")
         else:
-            print("Failed to scrape website)
+            print("Failed to scrape website")
    d  admdlyt vi vrhligtewbie
  tm.sep(admuiom1 )
 i _ae_= _mi_"
