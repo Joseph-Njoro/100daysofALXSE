f7425b5b2897ceeaa610aad543637e3f5434dd49
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-21 23:16:08 +0300

    fix

--- web_scrapper.py
+++ web_scrapper.py
@@ -63,7 +63,7 @@ def main():
         else:
             print("Failed to scrape website")
 
-        # Add a random dlyt vi vrhligtewbie
+        # Add a random delyt vi vrhligtewbie
  tm.sep(admuiom1 )
 i _ae_= _mi_"
 mi(
