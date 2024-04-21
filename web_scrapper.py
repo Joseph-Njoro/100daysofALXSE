b3a97898870d1e9ace33e3862ff247778ce2584f
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-21 23:18:14 +0300

    fix

--- web_scrapper.py
+++ web_scrapper.py
@@ -64,6 +64,6 @@ def main():
             print("Failed to scrape website")
 
         # Add a random delay to avoid overwhelming the websites
-        time.sleep(randomuiom1 )
+        time.sleep(random.uiom1 )
 i _ae_= _mi_"
 mi(
