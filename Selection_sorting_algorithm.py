abded239777b92b17159172cb2b9e09bf9e19ea1
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-22 22:13:23 +0300

    Sorting Algorithm

---
+++ Selection_sorting_algorithm.py
@@ -0,0 +1,16 @@
+def selection_sort(items):
+    """Sorts a list of items into ascending order using the
+       selection sort algoright.
+       """
+    for step in range(len(items)):
+        # Find the location of the smallest element in
+        # items[step:].
+        location_of_smallest = step
+        for location in range(step, len(items)):
+            # determine location of smallest
+            if items[location] < items[location_of_smallest]:
+                location_o_mlet=lcto
+   xhneiesse]wt tm[oaino_mlet
+ tmoayie  tm[tp
+ iesse tm[oaino_mlet
+  tm[oaino_mlet  eprr_tm
