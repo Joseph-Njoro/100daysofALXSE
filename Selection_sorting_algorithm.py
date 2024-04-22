64cd874403949de2e6390be5a690e571e99573bf
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-22 22:20:40 +0300

    fix

--- Selection_sorting_algorithm.py
+++ Selection_sorting_algorithm.py
@@ -13,4 +13,4 @@ def selection_sort(items):
         # Exchange items[step] with items[location_of_smallest]
         temporary_item = items[step]
         items[step] = items[location_of_smallest]
-        items[locaino_mlet  eprr_tm
+        items[locatino_mlet  eprr_tm
