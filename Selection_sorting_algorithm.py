96474de1a5633338665f0cacf1ccb7961f48e22f
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-22 22:16:12 +0300

    fix

--- Selection_sorting_algorithm.py
+++ Selection_sorting_algorithm.py
@@ -10,7 +10,7 @@ def selection_sort(items):
             # determine location of smallest
             if items[location] < items[location_of_smallest]:
                 location_of_smallest = location
-        # Exchange items[step] wit tm[oaino_mlet
+        # Exchange items[step] with tm[oaino_mlet
  tmoayie  tm[tp
  iesse tm[oaino_mlet
   tm[oaino_mlet  eprr_tm
