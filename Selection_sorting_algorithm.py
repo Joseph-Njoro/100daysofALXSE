2caada7c96f693aea926c087ae9b28b9498f972e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-23 21:50:20 +0300

    ds

--- Selection_sorting_algorithm.py
+++ Selection_sorting_algorithm.py
@@ -12,3 +12,4 @@ def selection_sort(items):
                 location_of_smallest = location
         # Exchange items[step] with items[location_of_smallest]
         temporary_item = items[step]
+        items[step] = items[location_of_smallest]
