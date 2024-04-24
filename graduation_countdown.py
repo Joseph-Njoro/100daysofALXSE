c05903acc57e35ce6e5d142c9849d09ab4e04688
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-24 20:58:00 +0300

    fix

--- graduation_countdown.py
+++ graduation_countdown.py
@@ -15,6 +15,7 @@ graduation_date = datetime.date(2024, 11, 1)
 def print_motivational_message():
 
     now = datetime.datetime.now()
+
     days_until_graduation = (graduation_date - now.date()).days
     motivational_messages = [
         "You got this!",
