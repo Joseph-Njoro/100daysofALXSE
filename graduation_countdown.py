3d4b12c84eed01f02a8a0cb9d7538b80132d875b
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-24 21:20:20 +0300

    fix

--- graduation_countdown.py
+++ graduation_countdown.py
@@ -6,27 +6,17 @@ import schedule
 
 
 # Set the graduation date
-
 graduation_date = datetime.date(2024, 11, 1)
 
-
 # Define a function to print the motivational message
-
 def print_motivational_message():
-
     now = datetime.datetime.now()
-
     days_until_graduation = (graduation_date - now.date()).days
-
     motivational_messages = [
-
         "You got this!",
-
         "Keep pushing forward!",
-
         "Believe in yourself!",
-
-        "You're almost there!",
+        "You're almost there!"
 
         "Don't give up now!",
 
