161ec044289f411a4fdf732cdc1ee67039a2ca49
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-25 22:12:43 +0300

    dewsq

--- hangman.c
+++ hangman.c
@@ -18,7 +18,7 @@ struct WordWithHint {
 // Function to display the current state of the word
 void displayWord(const char word[], const bool guessed[]);
 
-// Function to draw the hanman
+// Function to draw the hangman
 void drawHangman(int tries);
 // driver codint main()
 
