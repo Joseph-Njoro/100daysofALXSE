73a508187570f32e466ec31f7f36d1c5ff4850fc
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-26 22:50:01 +0300

    dsa

--- hangman.c
+++ hangman.c
@@ -108,7 +108,7 @@ void displayWord(const char word[], const bool guessed[])
 {
 	printf("Word: ");
 	for (int i = 0; word[i] != '\0'; i++) {
-	if (guessed[word[i] - 'a']) {
+		if (guessed[word[i] - 'a']) {
 		printf("%c ", word[i]);
 		
 		else {			printf("_ ");
