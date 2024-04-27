59a6ba0a6da8873f45521865fd15615f0e5c134e
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-27 23:15:39 +0300

    DSA

--- BMI.c
+++ BMI.c
@@ -6,8 +6,9 @@ tdio.h>int main()
     printf("Enter your wight in kilogra);
     scanf("%f", &weight);
     // Calculate BMI using the formua:weight / (height * height)
-    bmi = weight / (height * height  // Display the calculated BMI    printf("Your BMI is: %.2f\n", bmi);
-    // Interpret the BMI valu
+    bmi = weight / (height * height);
+    // Display the calculated BMI    printf("Your BMI is: %.2f\n", bmi);
+    // Interpret the BMI value
 if (bmi < 18.5) {
      printf("You are underweight.\n");
     } ese if (bmi >= 18.5 && bmi < 25) {
