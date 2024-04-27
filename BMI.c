0984cc18a6f31c4d17d68fd7dfb7d260564742a3
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-27 23:14:40 +0300

    FD

--- BMI.c
+++ BMI.c
@@ -15,5 +15,5 @@ if (bmi < 18.5) {
     } els if (bmi >= & bmi < 30) {
         prntf("You e overweight.\n");
     } else
-        pintf("You are obese.\n");    }
+        printf("You are obese.\n");    }
     return 0;
