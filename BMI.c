733cf8fab53615ebba0eb3792e5deef568d61d18
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-27 23:22:09 +0300

    DSA

--- BMI.c
+++ BMI.c
@@ -10,6 +10,7 @@ int main() {
     // Prompt the user to enter their weight in kilograms
     printf("Enter your weight in kilograms: ");
     scanf("%f", &weight);
+
     // Calculate BMI using the formua: weight / (height * height)
     bmi = weight / (height * height);
     // Display the calculated BMI    printf("Your BMI is: %.2f\n", bmi);
