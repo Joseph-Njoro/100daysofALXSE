b0e1ef55b28bb6d91cc0c138b431076e92c8b64a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:00:33 +0300

    dsa

--- BMI.c
+++ BMI.c
@@ -8,6 +8,7 @@ float promptFatconst char *message) {
         pintf("Please ente a poitive value: ");
         canf("%f", &value);    }   return value;
 // Function to calculate BMIfloat calculateBMI(loat height, float weight) {   return weight / (height * height);
+
 // Function tointerpret BMI nd print the result
 void interpretBI(float bmi) {    printf("Your BMI is: %.2f\n", bmi);
 if (bmi < 18.5) 
