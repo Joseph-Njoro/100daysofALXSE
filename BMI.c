21b68be5e035a032f282d3d71c63a8d4f77b0ee0
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 21:59:18 +0300

    fdwsqa

--- BMI.c
+++ BMI.c
@@ -27,6 +27,7 @@ if (bmi < 18.5)
         prtf("Focus on incorporating more fruits, vegetables, and lean proteins into your meals.\n");
     } els {        printf("Seek guidance from a healthcare professional to develop a weight loss plan.\n");
    
+
 int main() 
     // Prompt the user to enter their height and weigh
     float height = promptFloat("Enter your height in meters: ")
