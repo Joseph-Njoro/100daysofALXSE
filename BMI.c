c1fbb4c330e81605688e2b17136c0f5a1bcd17fb
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:02:19 +0300

    DSA

--- BMI.c
+++ BMI.c
@@ -16,7 +16,7 @@ float promptFlat(const char *message) {
 }
 // Function to interpret BMI nd print the result
 void interpretBMI(float bmi) {    printf("Your BMI is: %.2f\n", bmi);
-if (bmi < 18.5) 
+if (bmi < 18.5) {
     printf("You are underweight.\n);
     } ele if (bmi >= 18.5 && bmi < 5) {
         pintf("Your weight is normal\n");
