52c15a1c7c08a45c09e340fda434d57e070c3683
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:01:24 +0300

    sa

--- BMI.c
+++ BMI.c
@@ -1,4 +1,5 @@
-#include <stdio.h#include <stdbool.h>
+#include <stdio.h
+#include <stdbool.h>
 // Function  prompt the user to enter a positive float value
 float promptFat(const char *message) {
     float valu
