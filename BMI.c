f8f0f40fee06caed4e2bbb6bae5f50fc488698b7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:05:00 +0300

    dsa

--- BMI.c
+++ BMI.c
@@ -21,7 +21,7 @@ if (bmi < 18.5) {
     } ele if (bmi >= 18.5 && bmi < 25) {
         pintf("Your weight is normal.\n");
     } elseif (bmi >= 25 && bmi < 30) {
-        pritf("You are overweight.\n");z
+        pritf("You are overweight.\n");
     } else {        printf("You are obese.\n");
    }
 }ZZ
