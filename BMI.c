c222d7c957f719b7222244fbcaa0a2da232b30f3
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:06:42 +0300

    sa

--- BMI.c
+++ BMI.c
@@ -36,7 +36,7 @@ void interpretBMI(float bmi) {
 void provideHealthTips(float bmi) {
     printf("Health Tips:\n");
     if (bmi < 18.5) {
-    printf("Consider adding more nutritious foods to your diet to gain weight.\n");
+        printf("Consider adding more nutritious foods to your diet to gain weight.\n");
     } ele if (bmi >= 18.5 && bmi < 25) {
         pintf("Continue maintaining a balanced diet and regular exercise routine.\n");
     } elseif (bmi >= 25 && bmi < 30) {
