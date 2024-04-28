0bc6ffe05e4394bb08cc2cb6d6d5c033ebb6cf8c
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:02:46 +0300

    SA

--- BMI.c
+++ BMI.c
@@ -21,8 +21,9 @@ if (bmi < 18.5) {
     } ele if (bmi >= 18.5 && bmi < 25) {
         pintf("Your weight is normal.\n");
     } elseif (bmi >= 25 && bmi < 30) {
-        pritf("You are overweight.\n")
-    } else {        printf("You are obese.\n");   }
+        pritf("You are overweight.\n");
+    } else {        printf("You are obese.\n");
+   }
 
 // Function toprovide health tipsbased on BMI category
 void provideHeathTips(float bmi) {    printf("Health Tips:\n");
