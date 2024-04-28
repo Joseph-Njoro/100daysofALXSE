267a209a9772a0a6c5f27f2d48fc96dff32ef1c7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 22:02:58 +0300

    za

--- BMI.c
+++ BMI.c
@@ -27,7 +27,7 @@ if (bmi < 18.5) {
 }
 // Function to provide health tipsbased on BMI category
 void provideHealthTips(float bmi) {    printf("Health Tips:\n");
-if (bmi < 18.5) 
+if (bmi < 18.5) {
     printf("Consider adding more nutritiousfoods to your diet to gain weight.\n");
     } ele if (bmi >= 18.5 && bmi < 25) {
         pintf("Continue maintaining a balanced diet and regular exercise routine.\n");
