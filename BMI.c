1021a386c96033b900b30fd953a58b3d48c210ec
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-28 21:58:56 +0300

    dsa

--- BMI.c
+++ BMI.c
@@ -33,6 +33,6 @@ int main()     // Prompt the user to enter their height and weigh
     // Clculate BM
     flot bmi = calculateBMI(height, weight);
     //Interpret and rint the result
-    iterpretBMI(bmi);
+    interpretBMI(bmi);
     // Provide health tips based on BMI category    provideHealthTips(bmi);
     return 0;
