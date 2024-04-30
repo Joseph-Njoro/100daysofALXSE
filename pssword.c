aef7f19ea6ecf2076da1c32ede9fe8e9dc84bef7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-30 23:15:54 +0300

    FDSA

--- pssword.c
+++ pssword.c
@@ -36,6 +36,7 @@ char* generatePassword(int length, bool includeLowercase, bool includeUppercase,
         printf("Memory allocation failed.\n");
         return NULL;
     }
+
     for (int i = 0; i < length; i++) {        int category = getRandomInt(0, totalChars - 1);
         if (includeLowercase && category < strlen(lowercaseChars)) {
         password[i] = lowercaseChars[getRandomInt(0, strlen(lowercaseChars) - 1)];
