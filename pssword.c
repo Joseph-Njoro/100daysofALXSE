d44318a70b3a3efe36acf1f8bc481a7da58e69f7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-04-30 23:16:00 +0300

    gfdsa

--- pssword.c
+++ pssword.c
@@ -39,6 +39,7 @@ char* generatePassword(int length, bool includeLowercase, bool includeUppercase,
 
     for (int i = 0; i < length; i++) {
         int category = getRandomInt(0, totalChars - 1);
+
         if (includeLowercase && category < strlen(lowercaseChars)) {
         password[i] = lowercaseChars[getRandomInt(0, strlen(lowercaseChars) - 1)];
     } else if (includeUppercase && category < strlen(lowercaseChars) + strlen(uppercaseChars)) {
