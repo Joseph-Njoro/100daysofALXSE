ced8a90e56d6279adf8c050448c871db53bdc1e4
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-01 21:59:01 +0300

    cxz

--- fitness.c
+++ fitness.c
@@ -22,7 +22,7 @@ class WellnessCoach:
 class UserProfile:
     def __init__(self, name, age, gender, weight, height, fitness_level):
         self.name = name
-        self.ag  g
+        self.age = g
         self.gender = gener
         self.weight = weigt
         self.height = heigh
