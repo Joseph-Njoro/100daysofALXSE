31b89bfe7fcb23e95196cb51f6df800c26075c58
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-01 21:57:47 +0300

    k

--- fitness.c
+++ fitness.c
@@ -18,7 +18,7 @@ class WellnessCoach:
         # Provide personalized coaching based on user's goals and analyzed health data
         pass
 
-# Define a clas or user profile
+# Define a class or user profile
 class UserProfile:    def __init__(self, name, age, gender, weight, height, fitness_level):
     self.name = name
     sl.g  g
