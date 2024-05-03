f81d6b48a4617dda4a68666cb0f01a4afcab6ee7
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:13:23 +0300

    gfdsa

--- temp.c
+++ temp.c
@@ -2,7 +2,7 @@ include <stdio.h>
 / Function to convert Celsius to Fahrenheitdouble celsiusToFahrenheit(double celsius) {
   return (celsius * 9 / 5) + 32;
 
-/ Function to convert Fahrenheit to Celsiusdouble fahrenheitToCelsius(double fahrenheit) {
+// Function to convert Fahrenheit to Celsiusdouble fahrenheitToCelsius(double fahrenheit) {
    return (fahrenheit - 32) * 5 / 9;
 }
 // Function to convert Celsius to Kelvindouble celsiusToKelvin(double celsius) {
