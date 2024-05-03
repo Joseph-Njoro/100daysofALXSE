11a30808d243cbd8bf7471a07e1bc071470f680c
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:14:02 +0300

    FDS

--- temp.c
+++ temp.c
@@ -9,6 +9,7 @@ double celsiusToFahrenheit(double celsius) {
 double fahrenheitToCelsius(double fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
 }
+
 // Function to convert Celsius to Kelvindouble celsiusToKelvin(double celsius) {
    return celsius + 273.15;
 }
