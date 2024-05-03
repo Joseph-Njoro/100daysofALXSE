ade1b5282fab8705296a31b6a9c08003524a206a
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:13:53 +0300

    HGFDS

--- temp.c
+++ temp.c
@@ -4,6 +4,7 @@
 double celsiusToFahrenheit(double celsius) {
     return (celsius * 9 / 5) + 32;
 }
+
 // Function to convert Fahrenheit to Celsiusdouble fahrenheitToCelsius(double fahrenheit) {
    return (fahrenheit - 32) * 5 / 9;
 }
