2fd768c1625b5ad497897923d20b1caa08cfd1d1
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:13:47 +0300

    FDSA

--- temp.c
+++ temp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-// Function to convert Celsius to Fahrenheitdouble celsiusToFahrenheit(double celsius) {
+// Function to convert Celsius to Fahrenheit
+double celsiusToFahrenheit(double celsius) {
    return (celsius * 9 / 5) + 32;
 }
 // Function to convert Fahrenheit to Celsiusdouble fahrenheitToCelsius(double fahrenheit) {
