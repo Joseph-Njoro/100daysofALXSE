66cb15243ff40e3f34692fed96f1276dda58a463
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:13:59 +0300

    GFDA

--- temp.c
+++ temp.c
@@ -7,7 +7,7 @@ double celsiusToFahrenheit(double celsius) {
 
 // Function to convert Fahrenheit to Celsius
 double fahrenheitToCelsius(double fahrenheit) {
-   return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - 32) * 5 / 9;
 }
 // Function to convert Celsius to Kelvindouble celsiusToKelvin(double celsius) {
    return celsius + 273.15;
