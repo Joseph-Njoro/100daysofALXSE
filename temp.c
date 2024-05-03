eede0fa349ab6589a08da227ac4b5a1cf1d9e5d4
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-03 22:15:47 +0300

    FDS

--- temp.c
+++ temp.c
@@ -49,7 +49,7 @@ int main() {
             printf("Temperature in Kelvin: %.2f\n", celsiusToKelvin(temperature));
             break;
         case 'F':
-            printf("Temperature in Fahrenheit: %.2f\n", temperature)
+            printf("Temperature in Fahrenheit: %.2f\n", temperature);
             printf("Temperature in Celsius: %.2f\n", fahrenheitToCelsius(temperature));            printf("Temperature in Kelvin: %.2f\n", fahrenheitToKelvin(temperature));
         break;
     case 'K':
