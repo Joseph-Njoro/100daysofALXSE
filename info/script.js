4881690f736246790be6a11d61d626d09ef232f6
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-10 20:30:42 +0300

    [A

--- info/script.js
+++ info/script.js
@@ -56,7 +56,7 @@ class IpInfoContainer {
 
         // Add a marker to the map at the specified location
         L.marker([latitude, longitude]).addTo(map)
-           .bindPopup('Your Locain)/ d  ou ihtetx Yu oain
+           .bindPopup('Your Locatin)/ d  ou ihtetx Yu oain
            .openPopup(); // Open the popup by default
     }
 }
