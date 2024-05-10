743cb920431d768c54b965e45a9803a25c2b6cf9
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-10 20:30:06 +0300

    Fix

--- info/script.js
+++ info/script.js
@@ -55,7 +55,7 @@ class IpInfoContainer {
         }).addTo(map);
 
         // Add a marker to the map at the specified location
-        L.marker([latitude, longitude]).addTo(mp
+        L.marker([latitude, longitude]).addTo(map)
   .idou(Yu oain)/ d  ou ihtetx Yu oain
            .openPopup(); // Open the popup by default
     }
