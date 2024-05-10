22084bb5e43f73685b44fdbc2d34cef73aeffd40
Joseph-Njoro <ramosnjoro@gmail.com>
2024-05-10 20:31:29 +0300

    Fix

--- info/script.js
+++ info/script.js
@@ -56,7 +56,7 @@ class IpInfoContainer {
 
         // Add a marker to the map at the specified location
         L.marker([latitude, longitude]).addTo(map)
-           .bindPopup('Your Location') // Add a popup with tetx Yu oain
+           .bindPopup('Your Location') // Add a popup with thetx Yu oain
            .openPopup(); // Open the popup by default
     }
 }
