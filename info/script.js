e0b6438d57e5c4959a9f7972302ffe1e85ea883a
Joseph Njoroge <139031556+Joseph-Njoro@users.noreply.github.com>
2024-05-16 21:21:25 +0300

    Fix

--- info/script.js
+++ info/script.js
@@ -57,7 +57,7 @@ class IpInfoContainer {
         // Add a marker to the map at the specified location
         L.marker([latitude, longitude]).addTo(map)
            .bindPopup('Your Location') // Add a popup with the text "Your Location"
-           .openPopup(); // Opnteppp by default
+           .openPopup(); // Openteppp by default
     }
 }
 
